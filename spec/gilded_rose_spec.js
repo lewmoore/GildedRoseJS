c4dd76d6c897386f79315cbe50d197c1be0a72e8
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-27 13:18:15 +0000

    Add first basic passing test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,7 @@
 describe('Gilded Rose', function() {
-	it('should foo', function() {
-		const gildedRose = new Shop([new Item('foo', 0, 0)]);
-		const items = gildedRose.updateQuality();
-		expect(items[0].name).toEqual('fixme');
+	it('should decrease sellIn by 1 each day', function() {
+		const plainItem = new Shop([new Item('plainItem', 10, 10)]);
+		const item = plainItem.updateQuality();
+		expect(item[0].sellIn).toEqual(9);
 	});
 });

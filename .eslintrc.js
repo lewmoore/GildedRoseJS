3b7cb9411a51a16ce3b1fcbb4aec24262027d0d1
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-27 16:16:54 +0000

    Add jasmine to eslint env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		browser: true,
+		es6: true,
+		jasmine: true
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always']
+	}
 };

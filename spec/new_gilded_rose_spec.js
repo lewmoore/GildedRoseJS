4988e05d67bd5e9cd42fb9ffdbba3db5d705a852
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-28 12:21:04 +0000

    Add parralel code with passing test for Aged Brie quality increase

---
+++ spec/new_gilded_rose_spec.js
@@ -0,0 +1,18 @@
+// describe('Regular Item Class', function() {
+// 	it('should increase quality by 1', function() {
+// 		item = new Item('Aged Brie', 10, 10);
+// 		const plainItem = new AgedBrie(item);
+// 		console.log(plainItem.changeQuality());
+// 		expect(plainItem.quality).toEqual(11);
+// 	});
+// });
+
+var items = [new Item('Aged Brie', 10, 10)];
+var shop = new Shop(items);
+
+describe('Item is Aged Brie', function() {
+	it('Should increase quality by 1', function() {
+		shop.updateQuality(items);
+		expect(items[0].quality).toEqual(11);
+	});
+});
---
+++ src/new_gilded_rose.js
@@ -0,0 +1,33 @@
+class Item {
+	constructor(name, sellIn, quality) {
+		this.name = name;
+		this.sellIn = sellIn;
+		this.quality = quality;
+	}
+}
+
+class Shop {
+	constructor(items = []) {
+		this.items = items;
+	}
+
+	updateQuality(items) {
+		for (let i = 0; i < this.items.length; i++) {
+			this.updateItem(items[i]);
+		}
+	}
+
+	isAgedBrie(item) {
+		return 'Aged Brie' == item.name;
+	}
+
+	increaseQuality(item) {
+		item.quality += 1;
+	}
+
+	updateItem(item) {
+		if (item.name == 'Aged Brie') {
+			this.increaseQuality(item);
+		}
+	}
+}
